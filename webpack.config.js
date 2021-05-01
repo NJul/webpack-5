4573a65815f717cd33fad84917c775c10f8c095f
Nina NJul <nina.dev.web@gmail.com>
2021-05-01 19:27:14 +0300

    add entry, output

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,13 @@ const path = require('path');
 const HtmlWebpacPlugin = require('html-webpack-plugin');
 
 module.exports = {
+  entry: {
+    main: path.resolve(__dirname, './src/index.js'),
+  },
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
   plugins: [
     new HtmlWebpacPlugin({
       title: 'Webpack tutorial',
